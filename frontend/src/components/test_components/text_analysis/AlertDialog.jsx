c5ef11fdedd4d144eef5b8c89f0f7a996c7a685b
Siyan <gsy7418@gmail.com>
2024-09-23 22:55:18 +0800

    new file:   frontend/src/components/test_components/text_analysis/AlertDialog.jsx 	deleted:    frontend/src/components/test_components/text_analysis/AlertMessage.jsx

---
+++ frontend/src/components/test_components/text_analysis/AlertDialog.jsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export default function AlertDialog({ message, open, setOpen }) {
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert"
+        aria-describedby="alert"
+      >
+        <DialogTitle id="alert-title">{"Invalid Input"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="aler-description">{message}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Ok</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
