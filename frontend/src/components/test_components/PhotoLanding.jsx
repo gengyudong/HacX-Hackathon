31f2e33dbe1faaf796d910fa1fff1c50669dbdc9
Siyan <gsy7418@gmail.com>
2024-09-24 11:30:36 +0800

    deleted:    frontend/src/components/test_components/PhotoLanding.jsx 	deleted:    frontend/src/components/test_components/constants/testconstant.jsx

--- frontend/src/components/test_components/PhotoLanding.jsx
+++
@@ -1,9 +0,0 @@
-import { Box, Typography } from "@mui/material";
-
-export default function PhotoLanding() {
-    return (
-        <Box>
-        <Typography variant="h5">Photo Landing Page</Typography>
-        </Box>
-    );
-}
